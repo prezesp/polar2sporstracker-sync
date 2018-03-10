38371fcaebd9d52c5002904e75e22f5ebc6e6846
Piotr Walkiewicz <piotr.walkiewicz@yahoo.com>
2018-03-10 18:37:16 +0100

    Get max 10 activities from polar. Move agent logic to agent.py

--- agent.py
+++ agent.py
@@ -1,7 +1,20 @@
 from app import App
 from PyIOKitMatchingHandler import IOKitMatchingHandler
+import os
+import time
+import yaml
 
+# read configuration
+workdir = os.path.dirname(os.path.realpath(__file__))
+config = yaml.load(open(workdir+'/config.yml'))
+delay = int(config['SYNC_DELAY']) if 'SYNC_DELAY' in config else 80
+
+# handle macOS launch event
 handler = IOKitMatchingHandler()
 handler.handle()
+
+# gives your polar time to synchronize with web app after plugged in
+time.sleep(delay)
+
 app = App()
 app.run()
